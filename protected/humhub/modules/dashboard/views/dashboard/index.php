662834628e78519b8b70718decff57277795e73e
unknown <trinhhan322@gmail.com>
2024-12-31 11:39:20 +0700

    delete invisible profile func

--- protected/humhub/modules/dashboard/views/dashboard/index.php
+++ protected/humhub/modules/dashboard/views/dashboard/index.php
@@ -777,7 +777,7 @@ $canCreateSpace = $manager->can(new CreatePublicSpace()) || $manager->can(new Cr
                         <?php if ($canCreateSpace) : ?>
                         <li>
                             <div class="dropdown-footer">
-                                <a href="#" class="btn btn-info pr-3 pl-3" data-action-click="ui.modal.load"
+                                <a href="#" class="btn btn-info pr-3 pl-3x" data-action-click="ui.modal.load"
                                 data-action-url="<?= Url::to(['/space/create/create']) ?>">
                                     <i class="fa fa-plus m-0"></i>
                                 </a>
